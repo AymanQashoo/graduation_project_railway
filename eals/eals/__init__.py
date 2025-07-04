1c3b12742266b8faf5acdc4e73623538108c4623
AymanQashoo <165571338+AymanQashoo@users.noreply.github.com>
2025-07-05 01:17:05 +0300

    Update __init__.py

--- eals/eals/__init__.py
+++ eals/eals/__init__.py
@@ -2,5 +2,5 @@ import importlib.metadata
 
 from .eals import ElementwiseAlternatingLeastSquares, load_model
 
-__version__ = importlib.metadata.version("eals")
+__version__ = "1.0.0"          
 __all__ = ["ElementwiseAlternatingLeastSquares", "load_model", "__version__"]
